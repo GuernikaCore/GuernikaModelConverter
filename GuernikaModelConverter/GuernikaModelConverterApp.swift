c10c6162c294c946652597d65b97ed25446eccec
Guillermo Cique Fernández <guiyec@gmail.com>
2023-03-19 16:39:32 +0100

    Initial Commit

---
+++ GuernikaModelConverter/GuernikaModelConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuernikaModelConverterApp.swift
+//  GuernikaModelConverter
+//
+//  Created by Guillermo Cique Fernández on 19/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GuernikaModelConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
