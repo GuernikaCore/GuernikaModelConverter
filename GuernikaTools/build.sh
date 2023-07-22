1984a061542444943436b356175007e6856cc63a
Guillermo Cique Fernández <guiyec@gmail.com>
2023-07-22 18:40:29 +0200

    Fix build script

--- GuernikaTools/build.sh
+++ GuernikaTools/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pyinstaller GuernikaTools.spec --clean -y
+pyinstaller GuernikaTools.spec --clean -y --distpath "../GuernikaModelConverter"
 codesign -s - -i com.guiyec.GuernikaModelConverter.GuernikaTools -o runtime --entitlements GuernikaTools.entitlements -f "../GuernikaModelConverter/GuernikaTools"
